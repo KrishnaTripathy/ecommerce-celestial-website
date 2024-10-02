e6ec6b44d6106ea2fcee5711b7872392e9dad4f8
bit-web24 <bitweb24@gmail.com>
2024-10-02 21:30:54 +0530

    set max width

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -37,11 +37,11 @@ function Product() {
     }
 
     return (
-        <div className="p-10 w-full text-white">
+        <div className="max-w-[1200px] mx-auto p-10 text-white">
             <div className="p-10 bg-[#202938] rounded-2xl gap-20">
                 <div className="main-details flex flex-row gap-10 justify-start flex-wrap">
-                    <div> <img src={product.image} alt="ProductImage" className="w-[600px] h-[400px] rounded-md" /> </div>
-                    <div className="flex flex-col w-[600px] gap-5">
+                    <div> <img src={product.image} alt="ProductImage" className="w-auto h-auto max-w-[300px] max-h-[300px] rounded-md md:max-w-[900px] md:max-h-[600px] lg:max-w-[600px] lg:max-h-[500px]" /> </div>
+                    <div className="flex flex-col w-full gap-5">
                         <h1 className=" text-white font-bold text-6xl">{product.name}</h1>
                         <h2 className="text-[#79aaff] text-4xl font-semibold">${product.price}</h2>
                         <p>{product.description}</p>
@@ -53,16 +53,20 @@ function Product() {
                     </div>
                 </div>
 
-                <div className="specifications-and-components flex flex-row w-full mt-20 gap-14 justify-start flex-wrap">
+                <div className="specifications-and-components flex flex-row w-full mt-20 gap-14 justify-around flex-wrap">
                     <div>
                         <h1 className="text-3xl font-bold">Specifications</h1>
-                        <div className="flex flex-col gap-2 mt-10">
-                            {product.specifications.map(([key, value]) => (
-                                <div className="flex flex-row justify-between gap-28 md:gap-96">
-                                    <h3>{key}</h3>
-                                    <p className="text-right">{value}</p>
-                                </div>
-                            ))}
+                        <div className="overflow-x-auto mt-10">
+                            <table className="table-auto w-full text-left">
+                                <tbody>
+                                    {product.specifications.map(([key, value]) => (
+                                        <tr key={key} className="border-b border-gray-700">
+                                            <td className="px-4 py-2">{key}</td>
+                                            <td className="px-4 py-2">{value}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                     <div>
@@ -97,3 +101,4 @@ function Product() {
 }
 
 export default Product;
+
